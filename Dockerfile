c281b8c9101840061957698d46079516e640d998
Echo Wang <wgqi1126@qq.com>
2020-03-25 14:14:41 +0800

    update add es requirements

--- Dockerfile
+++ Dockerfile
@@ -3,7 +3,9 @@ FROM python:3.8.2-alpine3.11
 WORKDIR /app
 
 RUN apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/v3.11/main/ \
-        mariadb-dev=10.4.12-r0 g++=9.2.0-r3
+        mariadb-dev=10.4.12-r0 g++=9.2.0-r4
+
+RUN mkdir /logs && chmod o+w /logs
 
 COPY . /app
 
@@ -11,4 +13,6 @@ RUN pip install -r requirements.txt
 
 EXPOSE 8080
 
-CMD ["python", "/app/manage.py", "runserver", "0:8080"]
+ENTRYPOINT ["python", "manage.py", "migrate"]
+
+CMD ["python", "manage.py", "runserver", "0:8080"]
--- manage.py
+++ manage.py
@@ -1,9 +1,23 @@
 import http.server
 import socketserver
+import sys
 
-PORT = 8080
-Handler = http.server.SimpleHTTPRequestHandler
+if len(sys.argv) < 2:
+    raise Exception('need args')
 
-with socketserver.TCPServer(('', PORT), Handler) as httpd:
-    print('serving at port', PORT)
-    httpd.serve_forever()
+cmd = sys.argv[1]
+
+if cmd == 'migrate':
+    print('fake do migrate...')
+    exit()
+
+elif cmd == 'runserver':
+    PORT = 8080
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(('', PORT), Handler) as httpd:
+        print('serving at port', PORT)
+        httpd.serve_forever()
+
+else:
+    raise Exception('unknown cmd %s' % cmd)
